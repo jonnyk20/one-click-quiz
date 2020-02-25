b786c9fb3cb2979bfd4a8cfd645d01cd4311c2e8
Jonny Kalambay <jonnyk20@gmail.com>
2020-02-24 23:37:01 -0800

    Fixing proxy error

--- src/setupProxy.js
+++ src/setupProxy.js
@@ -1,5 +1,10 @@
-const proxy = require("http-proxy-middleware");
+const { createProxyMiddleware } = require("http-proxy-middleware");
 
 module.exports = app => {
-  app.use(proxy("/", { target: "http://localhost:8080", changeOrigin: true }));
+  app.use(
+    createProxyMiddleware("/", {
+      target: "http://localhost:8080",
+      changeOrigin: true
+    })
+  );
 };
